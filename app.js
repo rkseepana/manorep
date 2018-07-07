2f26382ae6e95dfa6aed4680ac668e1492fbe7d9
ram <rkseepana@yahoo.com>
2018-07-07 19:56:05 +0530

    db chnages
    mlab

--- app.js
+++ app.js
@@ -5,13 +5,34 @@ var faqRouter = require('./routers/faq');
 var dfRouter = require('./routers/webhook')
 
 var app = express();
-
+//var MongoClient = mongodb.MongoClient;
 var PORT = 8080;
-var HOST_NAME = 'localhost';
-var DATABASE_NAME = 'FAQ';
+var HOST_NAME = 'ds129821.mlab.com:29821';
+var DATABASE_NAME = 'faq';
 
-mongoose.connect('mongodb://' + HOST_NAME + '/' + DATABASE_NAME);
+//var url = 'mongodb://raju:abc123@ds129821.mlab.com:29821/faq?authMechanism=SCRAM-SHA-1';
+var url = process.env.MONGODB_URI;
+mongoose.connect(url, {
+  useMongoClient: true
+});
 
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//mongoose.connect('mongodb://rkseepana:@lordM369' +'@' + HOST_NAME + '/' + DATABASE_NAME);
+//var url = 'mongodb://localhost:27017/my_database_name'
+/*MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+   // db.close();
+  }
+});*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
